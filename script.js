ac72b5964fd0a8d554ae60fd0f29bad4ca59d9a5
Thiago Andrade <thiago.scss@proton.me>
2022-12-16 20:02:50 -0300

    Projeto Calculadora

---
+++ script.js
@@ -0,0 +1,25 @@
+const displayRes = document.querySelector(".display-result");
+const displayOp = document.querySelector(".display-operations");
+
+function insert(num) {
+  const number = displayRes.innerHTML;
+  displayRes.innerHTML = number + num;
+}
+
+function backspace() {
+  const backspace = displayRes.innerHTML;
+  displayRes.innerHTML = backspace.substring(0, backspace.length - 1);
+}
+
+function clean() {
+  displayOp.innerHTML = "";
+  displayRes.innerHTML = "";
+}
+
+function calc() {
+  if (displayRes) {
+    const calc = displayRes.innerHTML;
+    displayOp.innerHTML = displayRes.innerHTML;
+    displayRes.innerHTML = eval(calc);
+  }
+}
